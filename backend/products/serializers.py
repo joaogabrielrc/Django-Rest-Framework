5f2783ff6164395c757f99ecbf0809e5339d34c1
João Gabriel Cardoso <joaogabrielrcardoso@gmail.com>
2022-07-31 17:54:24 -0300

    Added validators in product serializer

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,11 +1,15 @@
+from wsgiref.validate import validator
 from rest_framework import serializers
 
 from .models import Product
+from .validators import validate_title
 
 
 class ProductSerializer(serializers.ModelSerializer):
   discount = serializers.SerializerMethodField(read_only=True)
   url = serializers.SerializerMethodField(read_only=True)
+  title = serializers.CharField(validators=[validate_title])
+
   class Meta:
     model = Product
     fields = [
@@ -24,4 +28,4 @@ class ProductSerializer(serializers.ModelSerializer):
     return object.get_discount()    
 
   def get_url(self, object):
-    return f'/api/products/{object.id}/'    
+    return f'/api/products/{object.id}/'      
