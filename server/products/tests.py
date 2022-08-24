e75cf1d116d1b719e560af29b1e5fb6d71dde7b8
João Gabriel Rozete Cardoso <joaogabrielrcardoso@gmail.com>
2022-08-24 00:09:02 -0300

    Add basic test in products app

--- server/products/tests.py
+++ server/products/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.test import APIClient
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+
+User = get_user_model()
+
+class UserPermissionsTestCase(TestCase):      
+  def test_admin_has_permission(self):
+    manager_a = User.objects.create_user(
+      username="user_a", 
+      password="pass123",
+      is_staff=True
+    )   
+    token = Token.objects.get_or_create(user=manager_a)[0]    
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+    endpoint = "/api/v1/products/"
+    response = client.get(endpoint)    
+    self.assertEqual(response.status_code, 200)  
+
+
+  def test_employee_has_permission(self):
+    employee_a = User.objects.create_user(
+      username="employee_a", 
+      password="pass123"
+    )
+    token = Token.objects.get_or_create(user=employee_a)[0]    
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
+    endpoint = "/api/v1/products/"
+    response = client.get(endpoint)    
+    self.assertEqual(response.status_code, 403)  
